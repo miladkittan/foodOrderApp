e90ef18be27b66092fa3fa9fc2e7ce69873c1ae4
Milad K <miladkittan1@gmail.com>
2024-11-02 20:38:15 +0200

    change url & img

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -12,7 +12,7 @@ export default function MealItem({meal}) {
 
     return ( <li className="meal-item">
         <article>
-            <img src={`${meal.image}`} alt={meal.name} />
+            <img src={`https://foodorderapp-g805.onrender.com/${meal.image}`} alt={meal.name} />
             <div>
                 <h3>{meal.name}</h3>
                 <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
